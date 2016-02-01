4c7a6ab2637859621b6ccfe34a751a09ce13d07a
helenetangen <helene.tangen@gmail.com>
2016-02-01 11:34:27 +0100

    Added and renamed variables.

--- Java/GeneticAlgorithm.java
+++ Java/GeneticAlgorithm.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class GeneticAlgorithm {
 
 	
+	// Genetic Algorithm Variables
+	protected WindFarmLayoutEvaluator evaluator;
+	protected boolean[][] individuals;
+	protected double[] fitness;
+	protected int populationSize;
+	protected int tournamentSize;
+	protected double mutationRate;
+	protected double crossoverRate;
+	protected ArrayList<double[]> grid;
 	
+	
+	// Other variables
+	protected Random random = new Random();
 	
 	
 }
