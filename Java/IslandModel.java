6661ad100a05088e5171ca2d37df79a1a94fd6f6
helenetangen <helene.tangen@gmail.com>
2016-02-02 13:04:15 +0100

    Implementing Island Model, each deme is an object of the GeneticAlgorithm class.

--- Java/IslandModel.java
+++ Java/IslandModel.java
@@ -1,49 +1,33 @@
 	
 	
-public class IslandModel extends GeneticAlgorithm{
+public class IslandModel{
 	
 	
-	private int demeCount;
-	private int demeSize;
-	private int migrationRate;
-	private int migrationInterval;
+	private int demeCount;         //Number of demes.
+	private int demeSize;          //Number of individuals in each deme.
+	private int migrationRate;     //Number of individuals that migrate.
+	private int migrationInterval; //Number of generations between migrations.
 	
 	
-	public IslandModel(WindFarmLayoutEvaluator evaluator, int populationSize, int tournamentSize, int generations, double mutationRate,double crossoverRate, int demeCount, int demeSize, int migrationRate, int migrationInterval) {
-		super(evaluator, populationSize, tournamentSize, generations, mutationRate,crossoverRate);
-		this.demeCount = demeCount;
-		this.demeSize = demeSize;
-		this.migrationRate = migrationRate;
+	//Each deme is a GeneticAlgorithm object.
+	private GeneticAlgorithm[] demes;
+	
+	
+	public IslandModel(WindFarmLayoutEvaluator evaluator, int populationSize, int tournamentSize, int generations, double mutationRate,double crossoverRate, int demeCount, int migrationRate, int migrationInterval) {
+		this.demeCount         = demeCount;
+		this.demeSize          = populationSize / demeCount;
 		this.migrationInterval = migrationInterval;
+		this.migrationRate     = migrationRate;
+		
+		demes = new GeneticAlgorithm[demeCount];
+		for (int i = 0; i < demeCount; i++){
+			demes[i] = new GeneticAlgorithm(evaluator, demeSize, tournamentSize, migrationInterval, mutationRate, crossoverRate);
+		}
+
 	}
 	
 	
-	public int[] tournamentSelection(){
-		int[] winners     = new int[populationSize/tournamentSize];
-		int[] competitors = new int[populationSize];
-		for (int c = 0; c < competitors.length; c++){
-			competitors[c] = c;
-		}
-		for (int c = 0; c < competitors.length; c++){
-			int index     = random.nextInt(c + 1);
-			int temporary = competitors[index];
-			competitors[index] = competitors[c];
-			competitors[c] = temporary;
-		}
-		for (int t = 0; t < winners.length; t++){
-			int winner = -1;
-			double winnerFitness = Double.MAX_VALUE;
-			for (int c = 0; c < tournamentSize; c++){
-				int competitor = competitors[tournamentSize * t + c];
-				if (fitness[competitor] < winnerFitness){
-					winner = competitor;
-					winnerFitness = fitness[winner];
-				}
-			}
-			winners[t] = winner;
-		}
-		return winners;
-	}
+	public 
 	
 	
 }
